5e47fdf9c6fe56c75f916b821858b24a3e7b2259
Travis Kehrli <tmktahu@gmail.com>
2025-06-16 22:23:18 -0700

    remove that stuff

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomBuffs
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "io.vrising.CustomBuffs";
-        public const string PLUGIN_NAME = "CustomBuffs";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
